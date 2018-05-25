1361de4d95bdb86583ab8bf3318fa7d9c3784e8e
Dani Lyn <dlyn1110@gmail.com>
2018-05-25 14:16:25 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,8 @@
 require 'pry'
 
 class Song
-  extend Memorable::ClassMethods
-  include Memorable::InstanceMethods
-  include Paramable
-  extend Findable
+  extend Memorable::ClassMethods, Findable::ClassMethods
+  include Memorable::InstanceMethods, Paramable::InstanceMethods
   
   attr_accessor :name
   attr_reader :artist
