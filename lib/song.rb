f62f5e1d4a113dfc14b5b39e45ddc2e11d4583dd
Dani Lyn <dlyn1110@gmail.com>
2018-05-25 14:05:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,8 +4,8 @@ class Song
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   include Paramable
-  extend Memorable
-
+  extend Findable
+  
   attr_accessor :name
   attr_reader :artist
 
